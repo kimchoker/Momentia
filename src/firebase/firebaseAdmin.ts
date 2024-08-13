958f2856f43096140aa8ba13f472384caf3dbadc
kimchoker <kimchoker@naver.com>
2024-08-13 15:27:02 +0900

    다시설정중

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 
 const firebaseAdminConfig = {
   privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/g, '\n'),
-  clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  clientEmail: process.env.CLIENT_EMAIL,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 };
 
