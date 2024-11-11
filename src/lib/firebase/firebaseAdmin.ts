c1838c5ecfa0acf83ba42f4e59e4fb0344c50d44
kimchoker <kimchoker@naver.com>
2024-11-11 15:04:09 +0900

    vercel 배포 관련 재수정

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 
 const firebaseAdminConfig = {
-  privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+  privateKey: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
   clientEmail: process.env.CLIENT_EMAIL,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 };
@@ -9,13 +9,13 @@ const firebaseAdminConfig = {
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(firebaseAdminConfig),
-    storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
+    storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`, // storageBucket 설정 확인
   });
 }
 
 const adminAuth = admin.auth();
 const adminDB = admin.firestore();
-const adminStorage = admin.storage().bucket();
-const messaging = admin.messaging(); // 서버에서 푸시 메시지를 전송할 때 사용
+const adminStorage = admin.storage().bucket(); // `bucket()` 메서드로 Firebase Storage 사용
+const adminMessaging = admin.messaging(); // 서버에서 푸시 메시지를 전송할 때 사용
 
-export { admin, adminAuth, adminDB, adminStorage, messaging };
+export { admin, adminAuth, adminDB, adminStorage, adminMessaging };
