a19f34b7b66249dcf016d327493820edc87e50a9
kimchoker <kimchoker@naver.com>
2024-11-11 16:05:03 +0900

    ㅇ

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -9,7 +9,7 @@ const firebaseAdminConfig = {
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(firebaseAdminConfig),
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    storageBucket: "snsproject-85107.appspot.com",
   });
 }
 
