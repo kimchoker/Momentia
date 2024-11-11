1d89bdeae17f69d094ae8b9924f1a8c772cf23d5
kimchoker <kimchoker@naver.com>
2024-11-11 17:54:09 +0900

    파이어베이스 수정

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,21 +1,24 @@
-import * as admin from 'firebase-admin';
+import * as firebaseAdminModule from 'firebase-admin';
 
-const firebaseAdminConfig = {
-  privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/g, '\n'),
-  clientEmail: process.env.CLIENT_EMAIL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-};
+export function getFirebaseAdmin() {
+  if (!firebaseAdminModule.apps.length) {
+    const firebaseAdminConfig = {
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.CLIENT_EMAIL,
+      projectId: process.env.PROJECT_ID,
+    };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(firebaseAdminConfig),
-    storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
-  });
+    firebaseAdminModule.initializeApp({
+      credential: firebaseAdminModule.credential.cert(firebaseAdminConfig),
+      storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+    });
+  }
+  return firebaseAdminModule;
 }
 
+// 필요한 곳에서 사용
+const admin = getFirebaseAdmin();
 const adminAuth = admin.auth();
 const adminDB = admin.firestore();
 const adminStorage = admin.storage().bucket();
-const messaging = admin.messaging(); // 서버에서 푸시 메시지를 전송할 때 사용
-
-export { admin, adminAuth, adminDB, adminStorage, messaging };
+const messaging = admin.messaging();
